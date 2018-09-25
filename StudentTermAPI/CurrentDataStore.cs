0bf3dacb4c3e171c2d8c8737816cbc8bec080533
Christopher Wong <Christopher.Wong.1@stonybrook.edu>
2018-09-25 11:43:37 -0400

    implemented feature 1 model and data store; working on controller

---
+++ StudentTermAPI/CurrentDataStore.cs
@@ -0,0 +1,61 @@
+﻿using StudentTermAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentTermAPI
+{
+    public class CurrentDataStore
+    {
+        public static CurrentDataStore Current { get; set; } = new CurrentDataStore();
+        public List<CurrentModel> Terms { get; set; }
+
+        public CurrentDataStore()
+        {
+            Terms = new List<CurrentModel>()
+            {
+                new CurrentModel()
+                {
+                    Months = new List<string>()
+                    {
+                        "September",
+                        "October",
+                        "November",
+                        "December"
+                    },
+                    Term = "Fall"
+                },
+                new CurrentModel()
+                {
+                    Months = new List<string>()
+                    {
+                        "January"
+                    },
+                    Term = "Winter"
+                },
+                new CurrentModel()
+                {
+                    Months = new List<string>()
+                    {
+                        "February",
+                        "March",
+                        "April",
+                        "May"
+                    },
+                    Term = "Spring"
+                },
+                new CurrentModel()
+                {
+                    Months = new List<string>()
+                    {
+                        "June",
+                        "July",
+                        "August",
+                    },
+                    Term = "Summer"
+                },
+            };
+        }
+    }
+}
---
+++ StudentTermAPI/Models/CurrentModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentTermAPI.Models
+{
+    public class CurrentModel
+    {
+        public List<string> Months
+        {
+            get;
+            set;
+        }
+        public string Term
+        {
+            get;
+            set;
+        }
+    }
+}
