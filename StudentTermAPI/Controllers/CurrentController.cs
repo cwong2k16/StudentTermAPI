0071db85fc66a603021403745939540144cd78a0
Christopher Wong <Christopher.Wong.1@stonybrook.edu>
2018-09-25 12:09:22 -0400

    finished feature 1. need to return only second object in JSON object.

--- StudentTermAPI/Controllers/CurrentController.cs
+++ StudentTermAPI/Controllers/CurrentController.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace StudentTermAPI.Controllers
 {
-    public class CurrentController
+    [Route("term/current")]
+    public class CurrentController: Controller
     {
+        [HttpGet()]
+        public JsonResult GetTerms()
+        {
+            string month = DateTime.Now.ToString("MMMM");
+
+            return new JsonResult(CurrentDataStore.Current.Terms.FirstOrDefault(
+                c=>c.Months.Contains(month)));
+        }
+
     }
 }
