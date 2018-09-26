a6a4e30ba0fd5705b19c2468e7b9e14bc79e32dd
Christopher Wong <Christopher.Wong.1@stonybrook.edu>
2018-09-26 11:06:14 -0400

    completed feature 2 ... reused model, data store, and where clause from feature 1

--- StudentTermAPI/Controllers/CurrentController.cs
+++ StudentTermAPI/Controllers/CurrentController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -19,9 +20,13 @@ namespace StudentTermAPI.Controllers
         }
 
         [HttpGet("{date:datetime:regex(\\d{{4}}-\\d{{2}}-\\d{{2}})}")]
-        public DateTime GetString(DateTime date)
+        public JsonResult GetString(DateTime date)
         {
-            return date;
+            int monthInt = date.Month;
+            string monthString = DateTimeFormatInfo.CurrentInfo.GetMonthName(monthInt);
+
+            return new JsonResult(CurrentDataStore.Current.Terms.Where(
+                t => t.Months.Contains(monthString)));
         }
 
     }
